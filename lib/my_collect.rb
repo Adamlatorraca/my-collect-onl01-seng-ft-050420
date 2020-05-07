d833634fac274f9371318929ee2b26f8e71c193a
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 19:00:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   first_names = []
   while i < array.length
-    yield(array[i]).split(" ").first
+    first_names << yield(array[i]).split(" ").first
     i += 1
   end
   first_names
